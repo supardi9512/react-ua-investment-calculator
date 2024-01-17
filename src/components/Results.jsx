8faf978d8097cc3340277941167d28c1880da75f
supardi_9512 <supardi@atomic.id>
2024-01-17 20:46:56 +0700

    computing values and properly handling number values

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,4 +1,8 @@
+import { calculateInvestmentResults } from '../util/investment';
+
 export default function Results({ input }) {
+    const resultsData = calculateInvestmentResults(input);
+
     return (
         <p>RESULT</p>
     );
